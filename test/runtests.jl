f35d9103136a9a612bdca67db54d7401164317b8
Carlo <carlo.tajoli@gmail.com>
2021-09-10 19:12:15 +0200

    My new test. Breaks things

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,11 +1,10 @@
 using MyExample
 using Test
 
-
-@test my_f(2,1) == 7
-
 @testset "MyExample.jl" begin
     #2x+3y
     @test my_f(2,1) == 7
     @test my_f(2,3) == 13
+    @test my_f(2,3) == 9
+
 end
